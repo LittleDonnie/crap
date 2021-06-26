b439f06e42ae135c6ae69febdd8ed05b4eb948d4
Monty Montgomery <poweredbyplants@gmail.com>
2021-06-26 16:04:27 -0500

    Initial Commit

---
+++ crap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  crap
+//
+//  Created by Monty Montgomery on 6/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ crap/crapApp.swift
@@ -0,0 +1,17 @@
+//
+//  crapApp.swift
+//  crap
+//
+//  Created by Monty Montgomery on 6/26/21.
+//
+
+import SwiftUI
+
+@main
+struct crapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
